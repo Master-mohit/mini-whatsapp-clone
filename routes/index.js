46ca9acd46fa903191d550f412225f903ff98162
Master-mohit <mohitmansigupta9098@gmail.com>
2024-02-28 01:33:06 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,57 @@
-var express = require('express');
-var router = express.Router();
+// Import necessary modules
+const express = require('express');
+const router = express.Router();
 const userModel = require("./users");
-const messageModel = require("./message");
 const passport = require("passport");
-const localstregy = require("passport-local"); // pass or username se bases me account bna ske..
-const users = require('./users');
+const localStrategy = require("passport-local");
 
-passport.use(new localstregy(userModel.authenticate()));  //logdin kr rhe ho pass or username ke basic me ...
+// Configure passport local strategy
+passport.use(new localStrategy(userModel.authenticate()));
 
-/* GET home page. */
+
+
+
+// GET home page route
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/home', function(req, res, next) {
-  const userLogdin = req.user  //logdin user 
-  res.render('home', {userLogdin})
-});
 
 
+// POST register route
 router.post("/register", function(req, res, next){
   const userData = new userModel({
-   username: req.body.username,
-   email: req.body.email,
+    username: req.body.username,
+    email: req.body.email,
   });
   userModel.register(userData, req.body.password)
-  .then ((result)=> {
-    passport.authenticate("local")(req, res, ()=> {
-      res.redirect("/home")
-     
-      
+    .then((result)=> {
+      passport.authenticate("local")(req, res, ()=> {
+        res.redirect("/home");
+      });
     })
-  })
-  .catch((err)=> {
-   res.send(err);
-  });
+    .catch((err)=> {
+      res.send(err);
+    });
 });
 
+// GET login page route
 router.get('/login', function(req, res, next) {
   res.render('login');
-  
 });
 
-router.post('/login', passport.authenticate("local", {//username or pass. ke baisc me login kroo..
-  successRedirect: "/home", //agr si data ho to use home route me bhej dena.
+router.get('/home', isLoggedIn ,async function(req, res) { 
+  const user = await userModel.findOne({username:req.session.passport.user}).populate('friend')
+  res.render('home',{user});
+});
+// POST login route
+router.post('/login', passport.authenticate("local", {
+  successRedirect: "/home",
   failureRedirect: "/login"
- }), (req, res, next) => { 
-   
- });
- 
- router.get('/logout', (req, res, next) => {
+}));
+
+// GET logout route
+router.get('/logout', (req, res, next) => {
   if (req.isAuthenticated())
     req.logout((err) => {
       if (err) res.send(err);
@@ -60,23 +62,13 @@ router.post('/login', passport.authenticate("local", {//username or pass. ke bai
   }
 });
 
- function isLoggedIn(req, res, next){
+// Middleware to check if user is authenticated
+function isLoggedIn(req, res, next){
   if(req.isAuthenticated()) return next();
-  res.redirect("/login")
- }
+  res.redirect("/login");
+}
 
-router.post("/searchUser", isLoggedIn, async function(req, res,next) {
-const data = req.body.data  
-res.send(req.body.data)
-const allUsers = await userModel.find({
-  username: {
-    $regex: data,
-    $options: 'i'
-  }
-})
-console.log(allUsers);
-res.status(200).json(allUsers)
 
-});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,12 @@ profileImage: {
   type: String,
   default: "https://plus.unsplash.com/premium_photo-1697232652942-5b43191a4015?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8"
 },
-socketId: String
+socketId: String, 
+
+friend:[{ 
+  type:mongoose.Schema.Types.ObjectId , 
+  ref:"user"
+}]
 });
 userSchema.plugin(plm);
 module.exports = mongoose.model("user", userSchema);
